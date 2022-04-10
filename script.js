ab550f2c7375df2819f26cbe7194ce552156416d
Jamshid <kalandarovjamshid01@gmail.com>
2022-04-11 00:46:50 +0500

    great

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let container = document.querySelector(".container");
 let big = document.querySelector(".big");
+let bpop = document.querySelector(".bpop");
 
 fetch("https://api.quran.sutanlab.id/surah")
   .then(function (response) {
@@ -27,32 +28,53 @@ fetch("https://api.quran.sutanlab.id/surah")
 container.addEventListener("click", function (e) {
   const btn = e.target;
   if (!btn.classList.contains("listen")) return;
-  console.log(btn.closest(".sura").classList.add("color"));
+  bpop.innerHTML = "";
+  let sura = btn.closest(".sura");
+  sura.classList.add("color");
+  let arr = [];
+  let nextSibling = sura.nextElementSibling;
+  let previousSibling = sura.previousElementSibling;
+  console.log(nextSibling);
+  console.log(previousSibling);
+  while (nextSibling) {
+    arr.push(nextSibling);
+    nextSibling = nextSibling.nextElementSibling;
+  }
+  while (previousSibling) {
+    arr.push(previousSibling);
+    previousSibling = previousSibling.previousElementSibling;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].classList.contains("color")) {
+      arr[i].classList.remove("color");
+    }
+  }
+  console.log(arr);
   const num = +btn.id;
   let html1, htm2;
   container.classList.add("gridcol");
   big.classList.add("grid");
-  document.querySelectorAll(".sura");
+
   fetch(`https://api.alquran.cloud/v1/surah/${num}/uz.sodik`)
     .then(function (response) {
       return response.json();
     })
     .then(function (res) {
       let data = res.data;
-      console.log(data);
+      // console.log(data);
       let getHtml = `<div class="popup">         
             <ion-icon class="close" name="close"></ion-icon>
             <div class="play"></div>
             <p class="name">${data.englishName}</p>           
           </div>`;
-      big.insertAdjacentHTML("beforeEnd", getHtml);
+      bpop.insertAdjacentHTML("afterBegin", getHtml);
       fetch(`https://api.quran.sutanlab.id/surah/${num}`)
         .then(function (response) {
           return response.json();
         })
         .then(function (res) {
           let data2 = res.data;
-          console.log(data2);
+          // console.log(data2);
           for (let i = 0; i < data2.verses.length; i++) {
             html1 = `<div class="text">
             <p class="arabcha">${data2.verses[i].text.arab}</p>
